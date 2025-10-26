8042d1e9bf64b1a77b5d6dd689ab52f8fa41654d
mda0030au <2s5s8cb9kr@privaterelay.appleid.com>
2025-10-26 12:59:24 -1000

    Add files via upload

---
+++ emojiappApp.swift
@@ -0,0 +1,17 @@
+//
+//  emojiappApp.swift
+//  emojiapp
+//
+//  Created by matt on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct emojiappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
